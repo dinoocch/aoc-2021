3647dfd799ceb59f639943c9c88e92d2f778d885
dinoocch <dino.occhialini@gmail.com>
2021-12-01 13:42:07 -0800

    Cleanup the code a bit incase someone looks at this lmao

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,5 @@
 use itertools::Itertools;
 
-
 #[aoc_generator(day1)]
 fn convert(input: &str) -> Vec<i32> {
     input
@@ -11,17 +10,21 @@ fn convert(input: &str) -> Vec<i32> {
 }
 
 #[aoc(day1, part1)]
-fn day1_part1_2021(input: &Vec<i32>) -> i32 {
-    input.iter().tuple_windows().map(|(previous, current)| {
-        if previous < current { 1 } else { 0 }
-    }).sum()
+fn day1_part1_2021(input: &Vec<i32>) -> usize {
+    input
+        .iter()
+        .tuple_windows()
+        .filter(|(previous, current)| previous < current)
+        .count()
 }
 
-
 #[aoc(day1, part2)]
-fn day1_part2_2021(input: &Vec<i32>) -> i32 {
-    input.iter().tuple_windows::<(_, _, _)>().map(|(a, b, c)| a + b + c).tuple_windows().map(|(previous, current)| {
-        if previous < current { 1 } else { 0 }
-    }).sum()
-
+fn day1_part2_2021(input: &Vec<i32>) -> usize {
+    input
+        .iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(previous, current)| previous < current)
+        .count()
 }
