b1dc258549b7eef56d57b447592b630e1672b58f
dinoocch <dino.occhialini@gmail.com>
2021-12-01 21:16:29 -0800

    Add day2

---
+++ src/day2.rs
@@ -0,0 +1,56 @@
+use itertools::Itertools;
+
+pub struct Day2 {}
+
+pub enum Direction {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+impl crate::aoc::AoCSolution<Vec<Direction>, i32> for Day2 {
+    fn day(&self) -> usize {
+        2
+    }
+
+    fn convert(&self, input: &str) -> Vec<Direction> {
+        input
+            .lines()
+            .map(|x| {
+                match x.split_once(' ') {
+                    Some(("forward", x)) => Direction::Forward(x.parse().unwrap()),
+                    Some(("down", x)) => Direction::Down(x.parse().unwrap()),
+                    Some(("up", x )) => Direction::Up(x.parse().unwrap()),
+                    Some((_, _)) => panic!(),
+                    None => panic!(),
+                }
+            })
+            .collect()
+    }
+
+    fn part1(&self, input: &Vec<Direction>) -> i32 {
+        let result = input
+            .iter()
+            .fold((0, 0), |(x, y), command| {
+                match command {
+                    Direction::Forward(n) => (x+n, y),
+                    Direction::Up(n) => (x, y-n),
+                    Direction::Down(n) => (x, y+n),
+                }
+            });
+        result.0 * result.1
+    }
+
+    fn part2(&self, input: &Vec<Direction>) -> i32 {
+        let result = input
+            .iter()
+            .fold((0, 0, 0), |(x, y, aim), command| {
+                match command {
+                    Direction::Down(n) => (x, y, aim + n),
+                    Direction::Up(n) => (x, y, aim - n),
+                    Direction::Forward(n) => (x+n, y+(aim * n), aim),
+                }
+            });
+        result.0 * result.1
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod aoc;
 mod day1;
+mod day2;
 
 use aoc::AoCSolution;
 
 fn main() {
-    day1::Day1 {}.run();
+    // day1::Day1 {}.run();
+    day2::Day2 {}.run();
 }
